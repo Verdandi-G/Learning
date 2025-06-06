011a78dafbcb8b7f064e97df35a3bbaa97714c42
Verdandi <verdandi.dev1@gmail.com>
2025-06-06 16:21:09 +0200

    created c# practice file

---
+++ 002.Learning.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Learning
+{
+    public class Learning
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Learning C# is fun!");
+            Console.WriteLine("Let's explore the basics of C# programming.");
+        }
+    }
+}
