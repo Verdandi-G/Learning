cc84b26bc1d2e6186d463cb01b81a775c0bb0a0a
Verdandi <verdandi.dev1@gmail.com>
2025-06-06 18:49:10 +0200

    debugged

--- 003.RandomNumber.cs
+++ 003.RandomNumber.cs
@@ -11,21 +11,25 @@ namespace Raten1
             Zahl = Zufall.Next(1, 101); // Zufallszahl zwischen 1 und 100
             Console.WriteLine("Willkommen zum Zahlraten!");
             Console.WriteLine("Ich habe eine Zahl zwischen 1 und 100 ausgewählt.");
-            Console.WriteLine("Versuche sie zu erraten!");
 
-            Eingabe = Console.ReadLine();
+
+            do {
+                Console.WriteLine("Versuche sie zu erraten!");
+                Eingabe = Convert.ToInt32(Console.ReadLine());
+                if (Eingabe < Zahl)
+            {
+                Console.WriteLine("Die gesuchte Zahl ist größer als deine Eingabe.");
+            }
+            if (Eingabe > Zahl)
+            {
+                Console.WriteLine("Die gesuchte Zahl ist kleiner als deine Eingabe.");
+            }
+            } while(Eingabe != Zahl);   
             if (Eingabe == Zahl)
             {
                 Console.WriteLine("Herzlichen Glückwunsch! Du hast die Zahl erraten.");
             }
-            else if (Eingabe < Zahl)
-            {
-                Console.WriteLine("Die gesuchte Zahl ist größer als deine Eingabe.");
-            }
-            else
-            {
-                Console.WriteLine("Die gesuchte Zahl ist kleiner als deine Eingabe.");
-            }
+            
             
         }
     }
