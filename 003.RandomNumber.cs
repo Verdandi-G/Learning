ccec7007573a1a2da49cb1e5d1cfb4f1a6de67ca
Verdandi <verdandi.dev1@gmail.com>
2025-06-06 18:39:22 +0200

    Numbers and if statements learned

---
+++ 003.RandomNumber.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Raten1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int Zahl, Eingabe;
+            Random Zufall = new Random();
+            Zahl = Zufall.Next(1, 101); // Zufallszahl zwischen 1 und 100
+            Console.WriteLine("Willkommen zum Zahlraten!");
+            Console.WriteLine("Ich habe eine Zahl zwischen 1 und 100 ausgewählt.");
+            Console.WriteLine("Versuche sie zu erraten!");
+
+            Eingabe = Console.ReadLine();
+            if (Eingabe == Zahl)
+            {
+                Console.WriteLine("Herzlichen Glückwunsch! Du hast die Zahl erraten.");
+            }
+            else if (Eingabe < Zahl)
+            {
+                Console.WriteLine("Die gesuchte Zahl ist größer als deine Eingabe.");
+            }
+            else
+            {
+                Console.WriteLine("Die gesuchte Zahl ist kleiner als deine Eingabe.");
+            }
+            
+        }
+    }
+}
